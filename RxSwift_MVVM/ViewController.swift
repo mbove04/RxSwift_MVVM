9a23bae8808db3b71b07e16660dd3559836a7935
Martin Bove <mbove04@gmail.com>
2020-06-11 16:13:57 +0200

    Initial Commit

---
+++ RxSwift_MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwift_MVVM
+//
+//  Created by Sailor on 11/06/2020.
+//  Copyright © 2020 com.sailor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
